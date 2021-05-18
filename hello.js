286fe0328d98097b02d51c6d64c8e25ef128df38
vs1232 <>
2021-05-18 22:36:19 +0000

    Added world

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
 function hello() {
 	// TODO: change this string so that your program prints "Hello world!" 
 	// when you hit the replit run button
-	return "Hello!";
+	return "Hello world!";
 }
  
 module.exports = hello;
